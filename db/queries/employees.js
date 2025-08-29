92d414eb7b5b2097cfeca7bb8ad78f54cd85c790
Seth Davis <sethdavis118@icloud.com>
2025-08-29 15:54:35 -0500

    Completed assignment

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,6 +1,14 @@
+import db from "#db/client";
+
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
   // TODO
+  const SQL = `
+  INSERT INTO employees (name, birthday, salary)
+  VALUES($1, $2, $3) RETURNING *
+  `;
+  const result = await db.query(SQL, [name, birthday, salary]);
+  return result.rows[0];
 }
 
 // === Part 2 ===
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { createEmployee } from "./queries/employees.js";
+import { faker } from "@faker-js/faker";
 
 await db.connect();
 await seedEmployees();
@@ -7,4 +9,12 @@ console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
   // TODO
+  for (let i = 0; i < 10; i++) {
+    const obj = {
+      name: faker.person.fullName(),
+      birthday: faker.date.birthdate(), // This may include a time when it shouldn't...
+      salary: faker.number.int({ min: 70000, max: 300000 }),
+    };
+    const movie = await createEmployee(obj);
+  }
 }
